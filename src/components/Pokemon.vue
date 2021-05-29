2a1902ab044ef23c9e06e8c1c3a9085d830ea677
V-Icaro <venicius.icarosr@gmail.com>
2021-05-29 13:48:33 -0300

    Trocando sprite do pokemon

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,25 +1,24 @@
 <template>
-  <div id="pokemon">
+  <div class="pokemon">
     <div class="card">
       <div class="card-image">
         <figure>
-          <img :src="pokemon.front" alt="Placeholder image" />
+          <img :src="currentImg" alt="Placeholder image" />
         </figure>
       </div>
       <div class="card-content">
         <div class="media">
-          <div class="media-left">
-            <figure class="image is-48x48">
-              <img :src="pokemon.front" alt="Placeholder image" />
-            </figure>
-          </div>
           <div class="media-content">
             <p class="title is-4">{{ num }} - {{ name | upper }}</p>
             <p class="subtitle is-6">{{ pokemon.type }}</p>
           </div>
         </div>
 
-        <div class="content"></div>
+        <div class="content">
+          <button class="button is-medium is-fullwidth" @click="mudarSprite">
+            Mudar sprite
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -34,10 +33,13 @@ export default {
       this.pokemon.type = res.data.types[0].type.name;
       this.pokemon.front = res.data.sprites.front_default;
       this.pokemon.back = res.data.sprites.back_default;
+      this.currentImg = this.pokemon.front;
     });
   },
   data() {
     return {
+      isFront: true,
+      currentImg: "",
       pokemon: {
         type: "",
         front: "",
@@ -56,11 +58,22 @@ export default {
       return newName;
     },
   },
+  methods: {
+    mudarSprite: function () {
+      if (this.isFront) {
+        this.isFront = false;
+        this.currentImg = this.pokemon.back;
+      } else {
+        this.isFront = true;
+        this.currentImg = this.pokemon.front;
+      }
+    },
+  },
 };
 </script>
 
 <style>
-#pokemon {
+.pokemon {
   margin-top: 10px;
 }
 </style>
